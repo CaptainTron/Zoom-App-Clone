2b4832f986b53bb3bc60fd835ef90af88cd02071
Vaibhav Yadav <tron21vaibhav@gmail.com>
2023-03-05 17:06:49 +0530

    Port Fixed

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ io.on('connection', socket =>{
         })
 })
 
-server.listen(3000, ()=>{
+server.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is Listening to port 3000......")
 })
